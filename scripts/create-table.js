60b09b21cac5486f3c3c5c0a51ba526e1fb36cbf
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-12-13 11:26:55 -0800

    server has been baked

---
+++ scripts/create-table.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const client = require('../lib/db-client');
+
+
+client.query(`
+CREATE TABLE IF NOT EXISTS profile (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(256) NOT NULL,
+    hash VARCHAR(256) NOT NULL
+  );
+`)
+    .then (
+        () => console.log('create tables script ran'), 
+        err => console.log(err)
+    )
+    .then(() => {
+        client.end();
+    });
