cd5606d569316b74dad6d3cc62868d6c2e8fd04a
JeiWolfei <jvampy22@gmail.com>
2018-12-13 11:26:44 -0800

    framework with Marty, need to dblcheck list

---
+++ scripts/drop-tables.js
@@ -0,0 +1,12 @@
+const client = require('../lib/db-client');
+
+client.query(`
+  DROP TABLE IF EXISTS profile;
+`)
+  .then(
+    () => console.log('drop tables complete'),
+    err => console.log(err)
+  )
+  .then(() => {
+    client.end();
+  });
