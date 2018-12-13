88390e7b5674ed5a99f544c1e082067fb2efffb1
martypdx <marty@martypdx.com>
2018-12-13 10:44:16 -0800

    working server

--- scripts/drop-tables.js
+++ scripts/drop-tables.js
@@ -1,7 +1,7 @@
 const client = require('../lib/db-client');
 
 client.query(`
-  DROP TABLE IF EXISTS game;
+  DROP TABLE IF EXISTS profile;
 `)
   .then(
     () => console.log('drop tables complete'),
