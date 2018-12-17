fa6996ef177a18310a366632a9d99ea24ace0afd
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-16 17:31:26 -0800

    able to see list of top 25 games being streamed on the app's UI

--- scripts/drop-tables.js
+++ scripts/drop-tables.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const client = require('../lib/db-client');
 
 client.query(`
-  DROP TABLE IF EXISTS animals;
+  DROP TABLE IF EXISTS profile;
   `)
   .then(
     () => console.log('drop tables complete'),
